e6bf565d9e867d5508d1ba90c2ddd7b01d49cc2b
julysun98 <58499801+julysun98@users.noreply.github.com>
2025-03-20 10:08:25 +0800

    Update train.py

--- train.py
+++ train.py
@@ -41,7 +41,6 @@ def train(config, training_chunked_samples_dir, testing_chunked_samples_file):
 
     model = SMACAN()
     model.to(config["device"])
-    # model = nn.DataParallel(model) #July
 
     optimizer = optim.Adam(model.parameters(), lr=config["lr"], eps=1e-7, weight_decay=0.0)
     scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[50], gamma=0.8)
@@ -77,17 +76,12 @@ def train(config, training_chunked_samples_dir, testing_chunked_samples_file):
                 model.train()
 
                 sample_frames, sample_ofs, boxs, _, _ = train_data  #motion(flow),ML_MemAE_SC reconstruction
-                # print(sample_ofs.shape) #(256,3,32,32)(B,C,H,W)
                 sample_ofs = sample_ofs.to(device)
                 sample_frames = sample_frames.to(device)
-                # print(sample_frames[:,0:12,:,:].shape)
-                # print(sample_ofs.shape)
 
                 out,out_Flow,_,_ = model(sample_frames[:,0:12,:,:], sample_ofs)
-                # print(out.shape)
                 loss_frame = intensity_loss(out, sample_frames[:,12:,:,:])
                 loss_grad = grad_loss(out, sample_frames[:,12:,:,:])
-                # print(out_Flow.shape,sample_ofs.shape)
                 loss_flow_recon = mse_loss(out_Flow, sample_ofs)
 
                 
@@ -115,62 +109,6 @@ def train(config, training_chunked_samples_dir, testing_chunked_samples_file):
         if epoch % config["saveevery"] == config["saveevery"] - 1:
             model_save_path = os.path.join(paths["ckpt_dir"], config["model_savename"])
             saver(model.state_dict(), optimizer.state_dict(), model_save_path, epoch + 1, step, max_to_save=300)
-
-
-def cal_training_stats(config, ckpt_path, training_chunked_samples_dir, stats_save_path):
-    device = config["device"]
-    model = SMACAN().to(config["device"]).eval()
-
-    # load weights
-    model_weights = torch.load(ckpt_path)["model_state_dict"]
-    model.load_state_dict(model_weights)
-    # print("load pre-trained success!")
-
-    score_func = nn.MSELoss(reduction="none")
-    training_chunk_samples_files = sorted(os.listdir(training_chunked_samples_dir))
-
-    of_training_stats = []
-    frame_training_stats = []
-
-    print("=========Forward pass for training stats ==========")
-    with torch.no_grad():
-
-        for chunk_file_idx, chunk_file in enumerate(training_chunk_samples_files):
-            dataset = Chunked_sample_dataset(os.path.join(training_chunked_samples_dir, chunk_file))
-            dataloader = DataLoader(dataset=dataset, batch_size=128, num_workers=0, shuffle=False)
-
-            for idx, data in tqdm(enumerate(dataloader),
-                                  desc="Training stats calculating, Chunked File %02d" % chunk_file_idx,
-                                  total=len(dataloader)):
-                sample_frames, sample_ofs, _, _, _ = data
-                # net_in = rearrange(sample_frames, 'b (c n) h w -> b c n h w', c=3)
-                # net_in = net_in[:,:,0:4,:,:]
-                sample_frames = sample_frames.to(device)
-                sample_ofs = sample_ofs.to(device)
-
-                out,out_flow,_,_ = model(sample_frames[:,0:12,:,:],sample_ofs)
-
-                loss_frame = score_func(out, sample_frames[:,12:,:,:]).cpu().data.numpy()
-                # print(loss_frame.shape)
-                loss_of = score_func(out_flow, sample_ofs).cpu().data.numpy()
-
-                of_scores = np.sum(np.sum(np.sum(loss_of, axis=3), axis=2), axis=1)
-                frame_scores = np.sum(np.sum(np.sum(loss_frame, axis=3), axis=2), axis=1)
-
-                of_training_stats.append(of_scores)
-                frame_training_stats.append(frame_scores)
-            del dataset
-            gc.collect()
-
-    print("=========Forward pass for training stats done!==========")
-    of_training_stats = np.concatenate(of_training_stats, axis=0)
-    frame_training_stats = np.concatenate(frame_training_stats, axis=0)
-
-    training_stats = dict(of_training_stats=of_training_stats,
-                          frame_training_stats=frame_training_stats)
-    # save to file
-    torch.save(training_stats, stats_save_path)
-
 
 if __name__ == '__main__':
     config = yaml.safe_load(open("./cfgs/CrossAttention.yaml")) ##july
